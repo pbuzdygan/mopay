69eb8bad87e09383ebcc0147d664810ce52c359b
pbuzdygan <pbuzdygan@outlook.com>
2026-01-05 00:55:00 +0100

    FIX: white page after last changes

--- frontend/src/components/modals/ExportModal.tsx
+++ frontend/src/components/modals/ExportModal.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ModalBase } from './ModalBase';
 import { useAppStore } from '../../store';
 import { Api } from '../../api';
